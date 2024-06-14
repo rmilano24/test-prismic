9829e0915f62b105fac94a224c182bdc1e863521
Ryan Milano <ryan@designbymilano.com>
2024-06-14 14:27:10 -0700

    make sure fade works

--- src/lib/slices/RichText/index.svelte
+++ src/lib/slices/RichText/index.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import {onMount} from "svelte";
+	import gsap from 'gsap';
 	import type { Content } from '@prismicio/client';
 	import { PrismicRichText } from '@prismicio/svelte';
 	import Label from './Label.svelte';
 
 	export let slice: Content.RichTextSlice;
+
+	onMount(()=> {
+		const tl = gsap.timeline({ defaults: {
+		ease: 'power2.inOut'}});
+
+		tl.fromTo(".fademe", { scale: 0.5 }, { scale: 1, opacity: 1, duration: 1.4});
+	});
+
 </script>
 
-<section class="container text-red-600">
+<section class="container text-red-600 fademe">
 	<PrismicRichText
 		field={slice.primary.content}
 		components={{
