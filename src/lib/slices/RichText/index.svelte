56a662f964eb1725c119bfc88ce6bb7189fa1cd6
Ryan Milano <ryan@designbymilano.com>
2024-06-14 14:22:58 -0700

    add gsap

--- src/lib/slices/RichText/index.svelte
+++ src/lib/slices/RichText/index.svelte
@@ -6,7 +6,7 @@
 	export let slice: Content.RichTextSlice;
 </script>
 
-<section class="container">
+<section class="container text-red-600">
 	<PrismicRichText
 		field={slice.primary.content}
 		components={{
